ec0ba24160d2c7b1e2c22007d3c32e212d749c7b
Joel Wangolo <wangoloj@hotmail.com>
2020-05-13 23:09:19 +0300

    Updated endpoint url

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 
+
 # spyse-subdomain-finder
-We all know about the famous spyse.com this python3 scripts scrounges [subdomains](https://www.nmmapper.com/sys/tools/subdomainfinder/) without the api.
-There is an option of getting the api, in fact that's what I recommend using the api they provide. Because this methods below may not bring out all [subdomains](https://www.nmmapper.com/sys/tools/subdomainfinder/) as the api can and perhaps the portability also.
+We all know about the famous spyse this python3 scripts scrounges subdomainswithout the api. There is an option of getting the api, in fact that's what I recommend using the api they provide. Because this methods below may not bring out all subdomains as the api can and perhaps the portability also.
 
 ## Installing dependencies
 You must have pip installed on your system
@@ -25,11 +25,8 @@ $ python3 spyse.py  -d nmmapper.com
 ]
 ```
 
-This tool is still in development. If you want a mature [subdomain finder](https://www.nmmapper.com/sys/tools/subdomainfinder/) either a script or [subdomain finders hosted online](https://www.nmmapper.com/sys/tools/subdomainfinder/) here are some details.
-## Other none api subdomain finder
-* [Censys none api](https://github.com/wangoloj/censys-subdomain-finder-non-api.git)
-* [Dnsdumpster script](https://github.com/wangoloj/dnsdumpster.git)
-* [8 Subdomain finder tools online](https://www.nmmapper.com/sys/tools/subdomainfinder/)
+This tool is still in development. If you want a mature  either a script or [subdomain finders hosted online](https://www.nmmapper.com/sys/tools/subdomainfinder/) here are some details.
+
 
 ## Cross-Selling
 * [Ethical-tools](https://ethicaltools.gitbook.io/subdomainfinder/)
--- spyse.py
+++ spyse.py
@@ -11,9 +11,11 @@ class SpyseSubdomainSearch(object):
 
     def __init__(self, domain):
         self.domain = self.cleanDomain(domain)
+        self.limit=20
+        self.offset=0
         
-        self.referer = "https://spyse.com/search/domain/{0}".format(self.domain)
-        self.host = "https://spyse.com/domain/json-subdomains?domain={0}&page=1&per_page=20&q={1}".format(self.domain, self.domain)
+        self.referer = "https://spyse.com/"
+        self.api_host="https://spyse.com/api/data/domain/subdomain?limit={0}&offset={1}&domain={2}".format(self.limit, self.offset, self.domain)
         self.host_header = "spyse.com"
         
         self.ua = useragents.UserAgents()
@@ -22,35 +24,40 @@ class SpyseSubdomainSearch(object):
                         "Accept-Language": "en-US,en;q=0.5",
                         "Accept-Encoding":"gzip, deflate, br",
                         "Connection": "keep-alive",
-                        "X-Requested-With":"XMLHttpRequest",
-                        "Accept":"*/*",
-                        "Referer":self.referer
+                        "Referer":self.referer,
+                        "Accept":"application/json, text/plain, */*"
                         }
         
         self.regx = r'(?:[a-zA-Z0-9](?:[a-zA-Z0-9\-]{,61}[a-zA-Z0-9])?\.)+[a-zA-Z]{2,6}'
         self.compiled = re.compile(self.regx)
-        self.results = None
+        self.results = {}
         self.subdomains = []
-        # re.findall(r"https?://(?!www\.)(.*\.nmmapper\.com).*", t)
-        # re.findall(r"(?!www\.)(.*\.nmmapper\.com).*", tx)
         
     def do_search(self):
         try:
-            res = requests.get(self.host, headers=self.headers)
-            self.results = res.content.decode('UTF-8')
+            res = requests.get(self.api_host, headers=self.headers)
+            if(res.status_code == 200):
+                self.results = res.json()
         except Exception as e:
             print(e)
             
     def get_hostnames(self):
-        rawres = spyseparser.SpyseParser(self.results, self)
-        rawres.run_parser()
         
-        subdomain_parsed = rawres.hostnames
+        if not self.results:
+            return self.subdomains 
+            
+        if not self.results.get("data"):
+            return self.subdomains 
+            
+        if not self.results.get("data").get("items"):
+            return self.subdomains
         
-        for sub in subdomain_parsed:
+        items = self.results.get("data").get("items")
+
+        for sub in items:
             self.subdomains.append({
-                "subdomain":sub,
-                "ip":self.resolve_ip(sub),
+                "subdomain":sub.get("name"),
+                "ip":self.resolve_ip(sub.get("name")),
                 "server":utils.get_server_type(sub)
             })
         return self.subdomains 
